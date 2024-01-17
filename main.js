ca04ee214460f1f6515bfef1da2681e2de763354
Artem <kolyasik124@ukr.net>
2024-01-17 21:02:03 +0200

    state controlLanguage

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ const btn = document.getElementById('btn');
 const text = document.getElementById('taskText');
 const imgAmendText = document.getElementById('change_img');
 const controlLanguage = document.getElementById('language');
-const english
-const 
+const english = document.getElementById('container__english');
+const ukraine = document.getElementById('container__ukr');
+let count = 0;
 let value;
+let state = false;
 const arrayLanguage = [container__english, container__ukr];
 fetch('https://6585c8f5022766bcb8c95bf4.mockapi.io/English', {
     method: 'Get',
@@ -25,8 +27,18 @@ fetch('https://6585c8f5022766bcb8c95bf4.mockapi.io/English', {
     });
 
 controlLanguage.onclick = function () {
-
+    english.classList.remove('border');
+    ukraine.classList.remove('border');
+    if (state) {
+        english.classList.add('border');
+        text.innerText = value[randomNumber].translation;
+    } else {
+        ukraine.classList.add('border');
+        text.innerText = value[randomNumber].word;
+    }
+    state = !state;
 };
+
 function randomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -34,19 +46,26 @@ let randomNumber; // Начальное случайное число
 
 imgAmendText.onclick = function () {
     randomNumber = randomInteger(1, 100);
-    console.log(randomNumber);
-
-    text.innerText = value[randomNumber].word;
+    if (state === true) text.innerText = value[randomNumber].word;
+    else text.innerText = value[randomNumber].translation;
 };
 
 btn.onclick = function () {
-    if (valueInput.value === value[randomNumber].translation) {
-        alert('Good');
+    if (state === true) {
+        if (valueInput.value === value[randomNumber].translation) alert('Good');
+        else alert(`${valueInput.value}не правильный ответ. Правильный ответ: ${value[randomNumber].translation}  `);
+        randomNumber = randomInteger(1, 100);
+        text.innerText = value[randomNumber].word;
     } else {
-        alert(`${valueInput.value}не правильный ответ. Правильный ответ: ${value[randomNumber].translation}  `);
-    }
-    randomNumber = randomInteger(1, 100);
+        if (valueInput.value === value[randomNumber].word) {
+            alert('Good');
+        } else {
+            alert(`${valueInput.value}не правильный ответ. Правильный ответ: ${value[randomNumber].word}  `);
+        }
+        randomNumber = randomInteger(1, 100);
 
-    text.innerText = value[randomNumber].word;
+        text.innerText = value[randomNumber].translation;
+    }
+
     valueInput.value = '';
 };
