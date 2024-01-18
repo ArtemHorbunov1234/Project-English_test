dc3fcd06b9ce11d20a948f9fb81319d42a97d359
Artem <kolyasik124@ukr.net>
2024-01-18 20:43:36 +0200

    count+, icon+, fix text, fix design

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 const valueInput = document.getElementById('myInput');
-const btn = document.getElementById('btn');
+const btnPush = document.getElementById('container_btn');
 const text = document.getElementById('taskText');
 const imgAmendText = document.getElementById('change_img');
 const controlLanguage = document.getElementById('language');
-const english = document.getElementById('container__english');
-const ukraine = document.getElementById('container__ukr');
-let count = 0;
+const english = document.getElementById('language__english');
+const ukraine = document.getElementById('language__ukr');
+const countCorrect = document.getElementById('count__correct');
+const btn = document.getElementById('btn');
+const display = document.getElementById('active_display');
+const iconActive = document.getElementById('normal_mode');
+const icon = document.getElementById('icon');
+const replacement = document.getElementById('replacement');
+const iconNormal = document.getElementById('icon_normal');
+const iconHard = document.getElementById('icon_hard');
+let num = 0;
 let value;
 let state = false;
-const arrayLanguage = [container__english, container__ukr];
+let randomNumber;
+let iconSwitch = true;
+let visibility;
 fetch('https://6585c8f5022766bcb8c95bf4.mockapi.io/English', {
     method: 'Get',
     headers: {
@@ -20,11 +30,18 @@ fetch('https://6585c8f5022766bcb8c95bf4.mockapi.io/English', {
     })
     .then((data) => {
         value = data;
-        console.log(data[0].translation);
     })
     .catch((error) => {
         console.error(error);
     });
+
+btn.onclick = function () {
+    btn.style.display = 'none';
+    display.style.display = 'flex';
+
+    randomNumber = randomInteger(1, 100);
+    text.innerText = value[randomNumber].translation;
+};
 
 controlLanguage.onclick = function () {
     english.classList.remove('border');
@@ -42,7 +59,6 @@ controlLanguage.onclick = function () {
 function randomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-let randomNumber; // Начальное случайное число
 
 imgAmendText.onclick = function () {
     randomNumber = randomInteger(1, 100);
@@ -50,22 +66,43 @@ imgAmendText.onclick = function () {
     else text.innerText = value[randomNumber].translation;
 };
 
-btn.onclick = function () {
+btnPush.onclick = function () {
     if (state === true) {
-        if (valueInput.value === value[randomNumber].translation) alert('Good');
-        else alert(`${valueInput.value}не правильный ответ. Правильный ответ: ${value[randomNumber].translation}  `);
+        if (valueInput.value === value[randomNumber].translation) alert('Good'), num++, (countCorrect.innerText = num);
+        else
+            alert(`${valueInput.value}не правильный ответ. Правильный ответ: ${value[randomNumber].translation}  `),
+                (num = 0),
+                (countCorrect.innerText = num);
         randomNumber = randomInteger(1, 100);
         text.innerText = value[randomNumber].word;
     } else {
-        if (valueInput.value === value[randomNumber].word) {
-            alert('Good');
-        } else {
-            alert(`${valueInput.value}не правильный ответ. Правильный ответ: ${value[randomNumber].word}  `);
-        }
+        if (valueInput.value === value[randomNumber].word) alert('Good'), num++, (countCorrect.innerText = num);
+        else
+            alert(`${valueInput.value}не правильный ответ. Правильный ответ: ${value[randomNumber].word} `),
+                (num = 0),
+                (countCorrect.innerText = num);
+
         randomNumber = randomInteger(1, 100);
 
         text.innerText = value[randomNumber].translation;
     }
 
     valueInput.value = '';
+};
+iconActive.onclick = function () {
+    visibility = iconSwitch ? 'block' : 'none';
+    icon.style.display = `${visibility}`;
+    iconSwitch = !iconSwitch;
+};
+
+iconNormal.onclick = function () {
+    replacement.innerText = 'Normal';
+    icon.style.display = 'none';
+    iconSwitch = !iconSwitch;
+};
+
+iconHard.onclick = function () {
+    replacement.innerText = 'Hard';
+    icon.style.display = 'none';
+    iconSwitch = !iconSwitch;
 };
