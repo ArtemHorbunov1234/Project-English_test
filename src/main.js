1ca5e1ed71f23eccebec70be37a8071acf39fa74
Artem <kolyasik124@ukr.net>
2024-01-26 20:26:12 +0200

    fix name id and class, history save info, statistics functions

--- src/main.js
+++ src/main.js
@@ -1,33 +1,53 @@
 import { value } from './dataFetcher.js';
 const valueInput = document.getElementById('myInput');
-const btnPush = document.getElementById('container_btn');
+const btnStartPush = document.getElementById('containerbtn');
 const text = document.getElementById('taskText');
-const imgAmendText = document.getElementById('change_img');
+const imgAmendText = document.getElementById('changeImg');
 const controlLanguage = document.getElementById('language');
-const english = document.getElementById('language__english');
-const ukraine = document.getElementById('language__ukr');
-const countBad = document.getElementById('count__bab');
-const countGood = document.getElementById('count__good');
-const btn = document.getElementById('btn');
-const display = document.getElementById('active_display');
-const iconActive = document.getElementById('normal_mode');
-const icon = document.getElementById('icon');
-const replacement = document.getElementById('replacement');
-const iconNormal = document.getElementById('icon_normal');
-const iconHard = document.getElementById('icon_hard');
-const iconAdd = document.getElementById('icon_add');
+const english = document.getElementById('languageEnglish');
+const ukraine = document.getElementById('languageUkr');
+const countBad = document.getElementById('countBabReply');
+const countGood = document.getElementById('countGoodReply');
+const btnStart = document.getElementById('btnStart');
+const display = document.getElementById('activeDisplay');
+const iconActive = document.getElementById('mode');
+const iconMode = document.getElementById('icon');
+const replacementMode = document.getElementById('replacementMode');
+const iconNormal = document.getElementById('iconNormalMode');
+const iconHard = document.getElementById('iconHardMode');
+const iconAddTeg = document.getElementById('iconAddTeg');
+const historyIcon = document.getElementById('historyIcon');
+const historyButton = document.getElementById('historyButton');
+const iconMeanValue = document.getElementById('iconMeanValue');
+const countCombo = document.getElementById('countCombo');
 let numGood = 0;
 let numBad = 0;
-let nomination;
 let state = false;
 let randomNumber;
-let iconSwitch = true;
-let visibility;
+let iconModeSwitch = true;
+let iconHistorySwitch = true;
+let visibilityIconMode;
+let visibilityIconHistory;
+let numberOfCorrectAnswers = 0;
+const tegLiLast = function (value_1, value_2, nomination, img) {
+    return `
+    <img width="18px" style="margin-left: 14px" src="/img/${img}.png" alt="" />
+    <b id="icon_nomination">${nomination}.</b><span>${valueInput.value}</span>
+    <img width="20px" src="/img/icon-right-arrow.png" alt="" />
+    <span>${value_1}</span>
+    <img width="20px" src="/img/icon-equal-mathematical-sign.png" alt="" />
+    <span >${value_2}</span>`;
+};
 
-btn.onclick = function () {
-    btn.style.display = 'none';
+const meanValueCalculator = (countReplyGood, countReplyBad) => {
+    const sumTask = countReplyBad + countReplyGood;
+    const meanValue = (countReplyGood / sumTask) * 100;
+    return meanValue.toFixed(2);
+};
+
+btnStart.onclick = function () {
+    btnStart.style.display = 'none';
     display.style.display = 'flex';
-
     randomNumber = randomInteger(1, 100);
     text.innerText = value[randomNumber].translation;
 };
@@ -55,66 +75,104 @@ imgAmendText.onclick = function () {
     else text.innerText = value[randomNumber].translation;
 };
 
-btnPush.onclick = function () {
+btnStartPush.onclick = function () {
     let liLast = document.createElement('li');
     if (state === true) {
         if (valueInput.value === value[randomNumber].translation) {
-            alert('Good'), numGood++, (countGood.innerText = numGood);
-            nomination = numBad + numGood;
-
-            liLast.innerHTML = `
-        <img width="18px" style="margin-left: 14px" src="/img/good.png" alt="" />
-        <b id="icon_nomination">${nomination}.</b><span>${valueInput.value}</span>
-        <img width="20px" src="/img/icon-right-arrow.png" alt="" />
-        <span>${value[randomNumber].word}</span>
-        <img width="20px" src="/img/icon-equal-mathematical-sign.png" alt="" />
-        <span >${value[randomNumber].translation}</span>`;
-            iconAdd.appendChild(liLast);
+            alert('Good'), numGood++, numberOfCorrectAnswers++, (countGood.innerText = numGood);
+            countCombo.innerText =
+                numberOfCorrectAnswers > countCombo.innerText ? numberOfCorrectAnswers : countCombo.innerText;
+            iconMeanValue.innerText = meanValueCalculator(numGood, numBad);
+            liLast.innerHTML = tegLiLast(
+                value[randomNumber].translation,
+                value[randomNumber].word,
+                numBad + numGood,
+                'good'
+            );
+            iconAddTeg.appendChild(liLast);
         } else {
             alert(`${valueInput.value}не правильный ответ. Правильный ответ: ${value[randomNumber].translation}  `),
                 numBad++,
                 (countBad.innerText = numBad);
-            nomination = numBad + numGood;
+            numberOfCorrectAnswers > countCombo.innerText ? numberOfCorrectAnswers : countCombo.innerText;
+            numberOfCorrectAnswers = 0;
+            iconMeanValue.innerText = meanValueCalculator(numGood, numBad);
 
-            liLast.innerHTML = `
-                <img width="18px" style="margin-left: 14px" src="/img/bad.png" alt="" />
-                <b id="icon_nomination">${nomination}.</b><span>${valueInput.value}</span>
-                <img width="20px" src="/img/icon-right-arrow.png" alt="" />
-                <span>${value[randomNumber].translation}</span>
-                <img width="20px" src="/img/icon-equal-mathematical-sign.png" alt="" />
-                <span >${value[randomNumber].word}</span>`;
-            iconAdd.appendChild(liLast);
+            liLast.innerHTML = tegLiLast(
+                value[randomNumber].translation,
+                value[randomNumber].word,
+                numBad + numGood,
+                'bad'
+            );
+            iconAddTeg.appendChild(liLast);
         }
+
         randomNumber = randomInteger(1, 100);
         text.innerText = value[randomNumber].word;
     } else {
-        if (valueInput.value === value[randomNumber].word) alert('Good'), numGood++, (countGood.innerText = numGood);
-        else
+        if (valueInput.value === value[randomNumber].word) {
+            alert('Good'), numGood++, numberOfCorrectAnswers++, (countGood.innerText = numGood);
+            iconMeanValue.innerText = meanValueCalculator(numGood, numBad);
+            countCombo.innerText =
+                numberOfCorrectAnswers > countCombo.innerText ? numberOfCorrectAnswers : countCombo.innerText;
+
+            liLast.innerHTML = tegLiLast(
+                value[randomNumber].word,
+                value[randomNumber].translation,
+                numBad + numGood,
+                'good'
+            );
+            iconAddTeg.appendChild(liLast);
+        } else {
             alert(`${valueInput.value}не правильный ответ. Правильный ответ: ${value[randomNumber].word} `),
                 numBad++,
                 (countBad.innerText = numBad);
+            console.log(countCombo.innerText);
+            numberOfCorrectAnswers > countCombo.innerText ? numberOfCorrectAnswers : countCombo.innerText;
+            numberOfCorrectAnswers = 0;
+            iconMeanValue.innerText = meanValueCalculator(numGood, numBad);
+
+            liLast.innerHTML = tegLiLast(
+                value[randomNumber].word,
+                value[randomNumber].translation,
+                numBad + numGood,
+                'bad'
+            );
+            iconAddTeg.appendChild(liLast);
+        }
 
         randomNumber = randomInteger(1, 100);
-
         text.innerText = value[randomNumber].translation;
     }
 
     valueInput.value = '';
 };
 iconActive.onclick = function () {
-    visibility = iconSwitch ? 'block' : 'none';
-    icon.style.display = `${visibility}`;
-    iconSwitch = !iconSwitch;
+    visibilityIconMode = iconModeSwitch ? 'block' : 'none';
+    iconMode.style.display = `${visibilityIconMode}`;
+    iconModeSwitch = !iconModeSwitch;
 };
 
 iconNormal.onclick = function () {
-    replacement.innerText = 'Normal';
-    icon.style.display = 'none';
-    iconSwitch = !iconSwitch;
+    replacementMode.innerText = 'Normal';
+    iconMode.style.display = 'none';
+    iconModeSwitch = !iconModeSwitch;
 };
 
-iconHard.onclick = function () {
-    replacement.innerText = 'Hard';
-    icon.style.display = 'none';
-    iconSwitch = !iconSwitch;
+historyButton.onclick = function (event) {
+    event.stopPropagation();
+    toggleHistoryIconVisibility();
 };
+
+document.body.onclick = function (event) {
+    if (!historyIcon.contains(event.target) && iconHistorySwitch === false) {
+        historyIcon.style.display = 'none';
+        iconHistorySwitch = true;
+    }
+};
+
+function toggleHistoryIconVisibility() {
+    visibilityIconHistory = iconHistorySwitch ? 'block' : 'none';
+    historyIcon.style.display = visibilityIconHistory;
+    iconHistorySwitch = !iconHistorySwitch;
+}
